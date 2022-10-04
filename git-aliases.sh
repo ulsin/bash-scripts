17f65231219983b56db7f8ad18be28756b44adfa
ulsin <ulriksin@gmail.com>
2022-10-04 22:04:25 +0200

    🤏 MINOR: misc

--- git-aliases.sh
+++ git-aliases.sh
@@ -151,8 +151,8 @@ alias gd='git diff'
 alias gds='git diff --staged'
 alias gdc='git diff --cached'
 
-alias gre='git restore'
-alias gres='git restore --staged'
+alias gre='git restore' # for restoring unstaged changes
+alias gres='git restore --staged' # for removing file from added
 
 alias ga='git add'
 alias gaa='git add --all --verbose'
