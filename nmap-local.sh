c54a3132df750b5c10cf324c815e1f97554cfa0e
ulsin <ulriksin@gmail.com>
2022-11-20 23:27:07 +0100

    👌 IMPROVE: improved nmap local

--- nmap-local.sh
+++ nmap-local.sh
@@ -1,9 +1,13 @@
 nmap-local() {
   network_address=$(ip route | tail -n 1 | cut -d" " -f1)
 
-  echo "Scanning lcoal network: ${network_address}"
+  echo "Scanning local network: ${network_address}"
 
   nmap_raport=$(nmap -sn $network_address)
 
-  echo "${nmap_raport}"
+  nmap_raport_grep=$(echo "$nmap_raport" | grep -E "Nmap scan report for|MAC Address:|Device type:")
+
+  nmap_raport_grep_cut=$(echo "$nmap_raport_grep" | cut -d" " -f5-)
+
+  echo "${nmap_raport_grep_cut}"
 }
